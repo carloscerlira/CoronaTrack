c1c99d8d0800c5dd5e9f491dd370db855f64cce2
carloscerlira <carloscerlira@gmail.com>
2020-09-20 03:35:37 -0500

    added all

---
+++ app.py
@@ -0,0 +1,20 @@
+import requests
+from flask import Flask, render_template, url_for, request
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/example')
+def example():
+    return render_template('exampleTemplate.html')
+
+@app.route('/country/<string:country>')
+def country(country):
+    data_url = f'https://raw.githubusercontent.com/carloscerlira/COVID-19-Tracker/master/data/world/{country}.json'
+    data = requests.get(data_url).json()
+    return render_template('countryInfo.html', data=data)   
+    
+if __name__ == '__main__':
+    app.run(debug=True)
