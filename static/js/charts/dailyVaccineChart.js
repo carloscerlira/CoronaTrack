577efa04f33e43c022d6e921a08ad50639e5582b
carlos <carloscerlira@gmail.com>
2021-02-12 00:47:10 -0600

    v1.0

---
+++ static/js/charts/dailyVaccineChart.js
@@ -0,0 +1,58 @@
+var dailyRecovered = {
+    chart: {
+    },
+    credits: {
+        enabled: false
+    },
+    title: {
+        text: 'Daily new recovered'
+    },
+    subtitle: {
+        text: general['country']
+    },
+    xAxis: {
+        type: 'datetime'
+    },
+    yAxis: {
+        title: false,
+        visible: false,
+        endOnTick: false,
+        min:0
+    },
+    tooltip: {
+        valueDecimals: 0
+    },
+    plotOptions: {
+        series: {
+            pointStart: general['start'],
+            pointInterval: 24*3600*1000
+        },
+        line: {
+            color: '#0056bf',
+        },
+        column: {
+            stacking: 'normal',
+            shadow: false,
+            pointPadding: 0,
+            borderWidth: 0,
+            crisp: false,
+        },
+        area: {
+            color: '#0056bf',
+            fillOpacity: 0.15
+        }
+    },
+    series: [
+        {
+            name: 'Daily new dosis',
+            type:'column',
+            data: time_series['daily_vaccine']
+        },
+        {
+            name: '7MA',
+            type:'area',
+            data: time_series['7MA_daily_vaccine']
+        }
+    ]
+}
+Highcharts.chart('dailyRecoveredChart', dailyRecovered);
---
+++ static/js/charts/vaccineChart.js
@@ -0,0 +1,53 @@
+var vaccineOptions = {
+    chart: {
+    },
+    credits: {
+        enabled: false
+    },
+    title: {
+        text: 'Total dosis given'
+    },
+    subtitle: {
+        text: general['country']
+    },
+    xAxis: {
+        type: 'datetime'
+    },
+    yAxis: {
+        title: false,
+        visible: false,
+        endOnTick: false,
+        min:0
+    },
+    tooltip: {
+        valueDecimals: 0
+    },
+    plotOptions: {
+        series: {
+            pointStart: general['start'],
+            pointInterval: 24*3600*1000
+        },
+        line: {
+            color: '#0056bf',
+        },
+        column: {
+            stacking: 'normal',
+            shadow: false,
+            pointPadding: 0,
+            borderWidth: 0,
+            crisp: false,
+        },
+        area: {
+            color: '#0056bf',
+            fillOpacity: 0.2
+        }
+    },
+    series: [
+        {
+            name: 'Total cases',
+            type:'area',
+            data: time_series['vaccine']
+        }
+    ]
+}
+Highcharts.chart('vaccineChart', vaccineOptions);
